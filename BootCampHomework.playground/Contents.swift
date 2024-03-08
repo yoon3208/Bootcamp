5901073060ab6374fe6e9d4d79b54dd4267235d1
yoonida <161405457+yoon3208@users.noreply.github.com>
2024-03-08 10:48:02 +0900

    Update Contents.swift

--- BootCampHomework.playground/Contents.swift
+++ BootCampHomework.playground/Contents.swift
@@ -1,4 +1,11 @@
 class Calculator{
+    let firstNumber: Double
+    let secondNumber: Double
+    
+    init(_ firstNumber: Double, _ secondNumber: Double) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
     func calculate(operator: String, firstNumber: Double, secondNumber: Double ) -> Double{
         switch `operator` {
         case "+":
@@ -9,25 +16,31 @@ class Calculator{
             return firstNumber * secondNumber
         case "/":
             return firstNumber / secondNumber
+//        case "%":
+//            return firstNumber.truncatingRemainder(dividingBy: secondNumber)
         default:
             return 0
         }
     }
         
-        func printResults() {
-        let calculator = Calculator()
+    func printResult(){
+    let calculator = Calculator (10, 20)
+        
         let addResult = calculator.calculate(operator: "+", firstNumber: 10, secondNumber: 20)
         let subtractResult = calculator.calculate(operator: "-", firstNumber: 10, secondNumber: 20)
         let multiplyResult = calculator.calculate(operator: "*", firstNumber: 10, secondNumber: 20)
         let divideResult = calculator.calculate(operator: "/", firstNumber: 10, secondNumber: 20)
+//        let remainderResult = calculator.calculate(operator: "%", firstNumber: 10, secondNumber: 20)
+      
         
    
         print("addResult : \(addResult)")
         print("subtractResult : \(subtractResult)")
         print("multiplyResult : \(multiplyResult)")
         print("divideResult : \(divideResult)")
+//       print("remainderResult : \(remainderResult)")
     }
 }
 let myObject = Calculator()
-myObject.printResults()
+myObject.printResult()
 
